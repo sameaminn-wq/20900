e2d68797eb2e60817c60b70fc174704a54084163
sameaminn-wq <sameaminn@gmail.com>
2026-03-14 01:13:55 +0200

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,24 @@
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import "./globals.css"
+import React from "react" // استيراد ريآكت للوصول إلى الأنواع
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body className="bg-gray-50">
+        <Navbar />
+        <main className="max-w-7xl mx-auto px-6 py-10">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
 
-return (
-
-<html lang="ar">
-
-<body className="bg-gray-50">
-
-<Navbar/>
-
-<main className="max-w-7xl mx-auto px-6 py-10">
-
-{children}
-
-</main>
-
-<Footer/>
-
-</body>
-
-</html>
-
-)
+  )
 
 }
